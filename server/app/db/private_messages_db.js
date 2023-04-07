88852c0859c47ecade6e87ff87ebe908c222c686
28720888 <eros.chan@etu.sorbonne-universite.fr>
2023-04-07 19:38:59 +0200

    add private messages db connections and rest

--- server/app/db/private_messages_db.js
+++ server/app/db/private_messages_db.js
@@ -1,10 +1,22 @@
 // dependencies
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const crypto = require('crypto');
+const { time } = require('console');
 
-// mongodb's stuff
+// mongodb stuff
 const URI = "mongodb+srv://Norras:Y1jGNQyOv8bZa0Sn@hame.jlet2.mongodb.net/?retryWrites=true&w=majority"; // todo : put credentials into env var
 const CLIENT = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const DB = CLIENT.db("Hame");
+const COLLECTION_NAME = "privatemessages";
+
+// local stuff
+const MESSAGE_TYPE = {
+  TEXT_MESSAGE: "Message",
+  URL: "URL",
+  VOICE: "Voice",
+  PICTURE: "Picture",
+  VIDEO: "Video"
+};
 
 
 
@@ -17,8 +29,8 @@ const DB = CLIENT.db("Hame");
  * @returns {Array} private conversations of user
  */
 async function getConversations(userId, request=false, limit=10) {
-  let collection = await DB.collection("privatemessages");
-  let query = {userId: userId, request: request};
+  let collection = await DB.collection(COLLECTION_NAME);
+  let query = {user_id: userId, request: request};
   let results = await collection.find(query)
     .limit(limit)
     .toArray();
@@ -26,22 +38,70 @@ async function getConversations(userId, request=false, limit=10) {
   return results;
 }
 
+async function createConversation(userId, participants, creationDate) {
+  let collection = await DB.collection(COLLECTION_NAME);
+  let newConversation = {
+    conversation_id: crypto.randomUUID(),
+    author_id: userId,
+    participants: participants,
+    creation_date: creationDate
+  };
+  let result = await collection.insertOne(newConversation);
+
+  return result;
+}
+
+/**
+ * Retrieve messages from database using conversationId
+ * @param {String} userId 
+ * @param {String} conversationId 
+ * @param {Int16Array} limit 
+ * @returns {Array} messages related to a conversation
+ */
 async function getMessages(userId, conversationId, limit=20) {
-  let collection = await DB.collection("privatemessages");
-  let query = {userId: userId, conversationId: conversationId};
+  let collection = await DB.collection(COLLECTION_NAME);
+  let query = {conversation_id: conversationId};
   let results = await collection.find(query)
-   .limit(limit)
-   .toArray();
+    .limit(limit)
+    .toArray();
+    
+  return results;
+}
+
+/**
+ * Send a message into a conversation using conversationId
+ * @param {Number} userId 
+ * @param {Number} conversationId 
+ * @param {String} content 
+ * @param {Enumerator} type 
+ * @param {Number} replyTo 
+ * @param {Boolean} isRead 
+ * @param {Date} sentDate 
+ * @returns 
+ */
+async function sendMessage(authorId, content, type, replyTo, isRead, conversationId, sentDate) {
+  let collection = await DB.collection(COLLECTION_NAME);
+  let newMessage = {
+    message_id: crypto.randomUUID(),
+    author_id: authorId,
+    content: content,
+    type: type,
+    reply_to: replyTo,
+    is_read: isRead,
+    conversation_id: conversationId,
+    sent_date: sentDate
+  };
+  let result = await collection.insertOne(newMessage);
   
-   return results;
+  return result;
 }
 
-function sendMessage(userId, conversationId) {
+async function deleteMessage(authorId, conversationId, messageId) {
+  let collection = await DB.collection(COLLECTION_NAME);
+  let query = {author_id: authorId, conversation_id: conversationId, message_id: messageId};
+  let result = await collection.deleteOne(query);
 
+  return result.deletedCount === 1;
 }
 
-function deleteMessage(userId, conversationId, messageId) {
-  
-}
-
-module.exports = { getConversations, getMessages, sendMessage, deleteMessage };
+module.exports = { getConversations, createConversation, getMessages, sendMessage, deleteMessage };
--- server/app/routes/private_messages.js
+++ server/app/routes/private_messages.js
@@ -9,33 +9,28 @@ const PRIVATE_MESSAGES_API = express.Router();
 
 
 /**
- * conversation structure :
+ * conversation schema :
  * {
- *   _id: ...
- *   conversation_id: ...
- *   author: user_id2
+ *   _id: ...,
+ *   conversation_id: ...,
+ *   author_id: user_id2,
  *   participants: [
  *     user_id1, user_id2
  *   ],
- *   messages: [
- *     {
- *       message_id: ...
- *       author: user_id1
- *       content: "voilà un message"
- *       reply_to: message_id
- *       is_read: boolean
- *       timestamp: ...
- *     },
- *     {
- *       message_id: ...
- *       author: user_id2
- *       content: "voilà une réponse"
- *       reply_to: message_id
- *       is_read: boolean
- *       timestamp: ...
- *     }
- *   ]
- *   timestamp: ...
+ *   creation_date: ...
+ * }
+ * 
+ * message schema:
+ * {
+ *   _id: ...,
+ *   message_id: ...,
+ *   author_id: user_id1,
+ *   content: "voilà un message",
+ *   type: ...,
+ *   reply_to: message_id,
+ *   is_read: boolean,
+ *   conversation_id: ...,
+ *   sent_date: ...
  * }
  */
 
@@ -52,28 +47,44 @@ PRIVATE_MESSAGES_API
   
   // get all requests for private conversations
   .get('/:userId/requests', async (req, res) => {
-    let result = pm.getConversations(req.params.userId, true);
+    let result = await pm.getConversations(req.params.userId, true);
 
     if (!result) res.status(400).json({message: "No requests for private conversations was found"});
     else res.status(200).json(result);
   })
+
+  // post a new conversation
+  .post('/:userId/conversations/', async (req, res) => {
+    let { participants, creation_date } = req.body;
+    let result = await pm.createConversation(req.params.userId, participants, creation_date);
+
+    if (!result) res.status(400).json({message: "Unable tso create a new conversation"});
+    else res.status(200).json({message: "New conversation was created successfully"});
+  })
  
   // get all messages from a conversation
   .get('/:userId/conversations/:conversationId/messages', async (req, res) => {
-    let result = pm.getMessages(req.params.userId, req.params.conversationId);
+    let result = await pm.getMessages(req.params.userId, req.params.conversationId);
 
-    if (!result) res.status(400).json({messages: "No messages was found"});
-    else res.status(200).json(result)
+    if (!result) res.status(400).json({message: "No messages was found"});
+    else res.status(200).json(result);
   })
 
   // post a new message
   .post('/:userId/conversations/:conversationId/messages/', async (req, res) => {
+    let { content, type, reply_to, is_read, sent_date } = req.body; 
+    let result = await pm.sendMessage(req.params.userId, content, type, reply_to, is_read, req.params.conversationId, sent_date);
 
+    if (!result) res.status(400).json({message: "Unable to post a new message"});
+    else res.status(200).json({message: "Message was posted successfully"});
   })
 
   // delete a message
   .delete('/:userId/conversations/:conversationId/messages/:messageId', async (req, res) => {
+    let result = await pm.deleteMessage(req.params.userId, req.params.conversationId, req.params.messageId);
 
+    if (!result) res.status(400).json({message: "Unable to delete message"});
+    else res.status(200).json({message: "Message was deleted successfully"});
   })
 
 module.exports = { PRIVATE_MESSAGES_API }
