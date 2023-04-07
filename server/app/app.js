694c6b2321d2f1e48f8276595ad68dd15de67ab6
28720888 <eros.chan@etu.sorbonne-universite.fr>
2023-04-07 10:43:12 +0200

    global var was renamed

--- server/app/app.js
+++ server/app/app.js
@@ -2,15 +2,15 @@
 const express = require('express');
 
 // handlers
-const { userAPI } = require('./handlers/users_handler');
-const { messageAPI } = require('./handlers/messages_handler');
-const { privateMessageAPI } = require('./handlers/private_messages_handler');
+const { USERS_API } = require('./handlers/users_handler');
+const { MESSAGES_API } = require('./handlers/messages_handler');
+const { PRIVATE_MESSAGES_API } = require('./handlers/private_messages_handler');
 
 // express' stuff
 const app = express();
 
 app
-  .use('/users', usersAPI)
-  .use('/messages', messagesAPI)
-  .use('/private-messages', privateMessagesAPI)
+  .use('/users', USERS_API)
+  .use('/messages', MESSAGES_API)
+  .use('/private-messages', PRIVATE_MESSAGES_API)
   .listen(8000);
