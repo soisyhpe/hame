792431bb415522f57e359700f390863bcacbbf69
28720888 <eros.chan@etu.sorbonne-universite.fr>
2023-05-07 21:35:29 +0200

    fix session issue

--- client/src/components/signin_form.js
+++ client/src/components/signin_form.js
@@ -1,14 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import '../assets/css/form.css'
 import logo from '../assets/medias/hame_logo.svg';
 
+import { EMAIL_REGEX, PASSWORD_REGEX } from '../tools/validation_tools';
+
 const Signin = () => {
+  const [userCredentials, setUserCredentials] = useState({
+    email: '',
+    password: '',
+  });
+  const [formErrors, setFormErrors] = useState({
+    email: '',
+    password: '',
+  });
   const navigate = useNavigate();
 
   useEffect(() => {
-    const response = fetch('http://localhost:8000/autoLogin', {
+    // test for email
+    if (userCredentials.email !== '' && !EMAIL_REGEX.test(userCredentials.email)) {
+      setFormErrors( prevValues => { return { ...prevValues, email: "Invalid email address" } });
+      document.getElementById("email").style.borderColor = "rgb(207, 94, 94)";
+    } else {
+      setFormErrors( prevValues => { return { ...prevValues, email: "" } });
+      document.getElementById("email").style.borderColor = null;
+    }
+
+    // test for password
+    if (userCredentials.password !== '' && !PASSWORD_REGEX.test(userCredentials.password)) {
+      setFormErrors( prevValues => { return { ...prevValues, password: 'Invalid password' } });
+      document.getElementById("password").style.borderColor = "rgb(207, 94, 94)";
+    } else {
+      setFormErrors( prevValues => { return { ...prevValues, password: "" } });
+      document.getElementById("password").style.borderColor = null;
+    }
+  }, [userCredentials]);
+
+  useEffect(() => {
+    const response = fetch('http://localhost:8000/v1/sessions/autoLogin', {
         method: "GET",
         credentials: "include",
       });
@@ -19,8 +50,23 @@ const Signin = () => {
       }
     }, []);
 
-  const handleSignin = () => {
-    const response = fetch('')
+  const updateUserCredentials = (event) => {
+    setUserCredentials( prevValues => { return { ...prevValues, [event.target.name]: event.target.value } });
+  }
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    axios.post('http://localhost:8000/v1/sessions/login', {
+        email: userCredentials.email,
+        password: userCredentials.password
+      })
+      .catch((err) => {
+        alert(err);
+      })
+      .then((response) => {
+        if (response.status === 200) navigate('/home');
+      });
   }
 
   return (
@@ -35,11 +81,26 @@ const Signin = () => {
             <div className='form-items'>
               <div className='form-item'>
                 <label className='form-item-label'>Adresse mail</label>
-                <input className='form-item-input' type='email' placeholder='prenom.nom@etu.sorbonne-universite.fr'></input>
+                <input 
+                  name='email'
+                  id='email'
+                  value={userCredentials.email}
+                  className='form-item-input' 
+                  type='email' 
+                  onChange={event => updateUserCredentials(event)} 
+                  placeholder='prenom.nom@etu.sorbonne-universite.fr'/>
+                <p id='email' className='form-item-error'>{ formErrors.email && formErrors.email }</p>
               </div>
               <div className='form-item'>
                 <label className='form-item-label'>Mot de passe</label>
-                <input className='form-item-input' type='password' placeholder='monsupermotdepasse'></input>
+                <input
+                  name='password'
+                  id='password'
+                  value={userCredentials.password}
+                  className='form-item-input'
+                  type='password'
+                  onChange={event => updateUserCredentials(event)}/>
+                <p id='password' className='form-item-error'>{ formErrors.password && formErrors.password }</p>
               </div>
               <div className='form-item'>
                 <a className='form-item-left' href='#'>Mot de passe oublié ?</a>
@@ -49,7 +110,7 @@ const Signin = () => {
           <div className='form-section'>
             <div className='form-items'>
               <div className='form-item'>
-                <button className='form-item-button'>Connexion</button>
+                <button id='submit' type='submit' className='form-item-button'>Connexion</button>
               </div>
             </div>
           </div>
